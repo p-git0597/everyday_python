c49daccac776a842dcc15dc9ccb037a680a7d3ae
p-git0597! <pbshah0597@gmail.com>
2024-01-31 17:30:52 -0500

    name and main intro

---
+++ 100_days_python/basic_coding/importing_library.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+# dt = pd.read_csv()
+
+# import math
+from math import sqrt, pi
+
+result = sqrt(9) * pi 
+print(result)
+
+from example import welcome, bank
+
+welcome()
+print(bank)
