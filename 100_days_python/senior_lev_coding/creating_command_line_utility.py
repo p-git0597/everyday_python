00fc213a3d0b3cf97474572621d7518853d1c927
p-git0597! <pbshah0597@gmail.com>
2024-02-08 16:47:59 -0500

    more prac on oops

---
+++ 100_days_python/senior_lev_coding/creating_command_line_utility.py
@@ -0,0 +1,28 @@
+import argparse
+import requests
+
+def download_file(url, local_filename):
+    if local_filename is None:
+        local_filename = url.split('/')[-1]
+    
+    with requests.get(url, stream = True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
+
+parser = argparse.ArgumentParser()
+
+# add commmand line arguments
+parser.add_argument("url", help="Url of the file to download")
+# parser.add_argument("output", help = "by which name do you want to save the file")
+parser.add_argument('-o', "--output", type = str, help="Name of the file", default=None)
+
+# parse the arguments
+args = parser.parse_args()
+
+# use the arguments in your code
+print(args.url)
+print(args.output, type(args.output))
+download_file(args.url, args.output)
